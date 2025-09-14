565cf4f302a45ad51f5809d8eeec9867203b4e4e
Vorkytaka <vorkytaka@gmail.com>
2025-09-15 02:09:19 +0300

    Add SVG icons to links in a terminal-like vertical list

---
+++ src/components/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as GitHub } from './GitHub.astro';
+export { default as LinkedIn } from './LinkedIn.astro';
+export { default as Email } from './Email.astro';
+export { default as Resume } from './Resume.astro';
