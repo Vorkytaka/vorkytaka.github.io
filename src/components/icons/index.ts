470e45b6aca5dae98596cfa6708cc9e26affb32d
Vorkytaka <vorkytaka@gmail.com>
2025-09-16 00:18:42 +0300

    Update site with new social links and styling improvements

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -2,3 +2,5 @@ export { default as GitHub } from './GitHub.astro';
 export { default as LinkedIn } from './LinkedIn.astro';
 export { default as Email } from './Email.astro';
 export { default as Resume } from './Resume.astro';
+export { default as Twitch } from './Twitch.astro';
+export { default as Telegram } from './Telegram.astro';
