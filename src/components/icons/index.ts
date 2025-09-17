19b4f3a14bcfaa53e0e8041b62cbc01af23855a3
Vorkytaka <vorkytaka@gmail.com>
2025-09-18 02:30:31 +0300

    Add YouTube icon and update related files

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -4,3 +4,4 @@ export { default as Email } from './Email.astro';
 export { default as Resume } from './Resume.astro';
 export { default as Twitch } from './Twitch.astro';
 export { default as Telegram } from './Telegram.astro';
+export { default as YouTube } from './YouTube.astro';
